3221a834713772f8edd4834ec6f11688ce410ac8
pferrando <30301059+pferrando@users.noreply.github.com>
2018-02-09 20:40:07 +0100

    Update scroll-blogger0.js

--- js/scroll-blogger0.js
+++ js/scroll-blogger0.js
@@ -1,10 +1,14 @@
 if(/^\/[0-9]{4}\/[0-9]{2}\//.test(document.location.pathname)){
+		var labels = document.getElementsByClassName('label-name')[0].innerHTML
+		for (var i = 1; i < document.getElementsByClassName('label-name').length; i++) {
+    			labels = labels + '-' + document.getElementsByClassName('label-name')[i].innerHTML
+		};
 		ga('ec:addProduct', {
 	   	'id': document.location.pathname,
 	   	'name': document.location.pathname,
-	   	'category': 'Apparel',
-	   	'brand': 'Google',
-	   	'variant': 'black',
+	   	'category': labels,
+	   	//'brand': 'brand',
+	   	//'variant': 'variant',
 	   	'price': '1',
 	   	'quantity': 1
 		});
