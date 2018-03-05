74386322a56ee6e2d9070d1ec19e884acfae8c68
pferrando <30301059+pferrando@users.noreply.github.com>
2018-03-06 00:38:12 +0100

    Update scroll-blogger.js

--- js/scroll-blogger0.js
+++ js/scroll-blogger0.js
@@ -1,7 +1,7 @@
 if(/^\/[0-9]{4}\/[0-9]{2}\//.test(document.location.pathname)){
 		var labels = document.getElementsByClassName('byline post-labels')[0].getElementsByTagName('a')[0].innerHTML
 		for (var i = 1; i < document.getElementsByClassName('byline post-labels')[0].getElementsByTagName('a').length; i++) {
-    			labels = labels + '-' + document.getElementsByClassName('byline post-labels')[0].getElementsByTagName('a')[i].innerHTML
+    			labels = labels + '_' + document.getElementsByClassName('byline post-labels')[0].getElementsByTagName('a')[i].innerHTML
 		};
 		ga('ec:addProduct', {
 	   	'id': document.location.pathname,
